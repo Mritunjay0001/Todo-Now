ec94476a823856bbaf13e5825f61b69c689c9c5b
Mritunjay Kumar <akshaysingh906022@gmail.com>
2023-08-15 00:58:22 +0530

    todo added

--- todo/models/todo.js
+++ todo/models/todo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const TodoSchema = mongoose.Schema(
   {
     task: { type: String, required: true },
-    compelete: { type: Boolean, required: true },
+    complete: { type: Boolean, required: true },
   },
   { timestamps: true },
 )
